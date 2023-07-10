5b1c1032df91aab323b5c4b4dd013d33975c8f21
rvlt1 <rvlt135@gmail.com>
2023-07-10 21:57:56 +0200

    add nft mint api test

--- blockchain_clients/w3_client.py
+++ blockchain_clients/w3_client.py
@@ -8,16 +8,18 @@ from data.config import (
     PRIVATE_TEST_KEY,
     URL_TESTNET_POLYGON
 )
+
+
 # from data.test_data import transaction_deploy_dict
 
 class w3Client:
     """Main client for work blockchain net"""
+
     def __init__(self, url: str,
                  contract: str,
                  contract_abi: str,
                  account: str,
                  private_key: str):
-
         self.url = url
         self.contract_address = contract
         self.contract_abi = contract_abi
@@ -78,7 +80,7 @@ class w3Polygon(w3Client):
 
         # Подпись и отправка транзакции
         signed_txn = self.w3_client.eth.account.sign_transaction(transaction_data_for_deploy_collection,
-                                                         private_key=PRIVATE_TEST_KEY)
+                                                                 private_key=PRIVATE_TEST_KEY)
         tx_hash = self.w3_client.eth.send_raw_transaction(signed_txn.rawTransaction)
 
         # Ожидание подтверждения транзакции
@@ -90,3 +92,28 @@ class w3Polygon(w3Client):
         else:
             print("Ошибка при выполнении транзакции.")
 
+    def transaction_mint_nft(self, collection_id: str,
+                             addresses_account_mint: str,
+                             tokenId: str,
+                             test_data_transaction: dict):
+        contract = self.w3_client.eth.contract(APP_CONTRACT_ADDRESSES, abi=ABI_NFT_CONTRACT_TEST)
+
+        # Создание транзакции
+        transaction_data_for_deploy_collection = contract.functions.deployCollection(collection_id,
+                                                                                     addresses_account_mint,
+                                                                                     tokenId).build_transaction(
+            test_data_transaction)
+
+        # Подпись и отправка транзакции
+        signed_txn = self.w3_client.eth.account.sign_transaction(transaction_data_for_deploy_collection,
+                                                                 private_key=PRIVATE_TEST_KEY)
+        tx_hash = self.w3_client.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+        # Ожидание подтверждения транзакции
+        tx_receipt = self.w3_client.eth.wait_for_transaction_receipt(tx_hash)
+
+        # Проверка статуса транзакции
+        if tx_receipt['status']:
+            print("Транзакция на отправку NFT успешно выполнена.")
+        else:
+            print("Ошибка при выполнении транзакции на отправку NFT ")
